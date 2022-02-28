e7428a4110e14d9abd94fee784b2b5abf4e4565f
jonatan de oliveira <j.jhonis95@gmail.com>
2022-02-28 18:17:59 -0400

    fixing the behavior of input name and card number

--- script.js
+++ script.js
@@ -8,12 +8,8 @@ const ccicon = document.getElementById('ccicon');
 const ccsingle = document.getElementById('ccsingle');
 const generatecard = document.getElementById('generatecard');
 
-const creditCard={
-    nome:'',
-    number:0000000000000000,
-    expiration:0000,
-    cvc:000
-}
+
+let numeros
 
 const svgname=document.getElementById('svgname');
 const svgnumber=document.getElementById('svgnumber');
@@ -22,20 +18,35 @@ nome.addEventListener('input',(e)=>{
     svgname.textContent=`${e.target.value}`
     svgname.textContent===``?svgname.textContent='JOHN DOE':'';
 })
-let arrNum=[]
+
 cardnumber.addEventListener('input',(e)=>{
-    let arrlength=arrNum.length;
-    arrNum=e.target.value
-    
-    switch(arrlength){
-        case 3:
-            arrNum=arrNum.slice(3,0)
-            console.log(arrNum)
-            break;
-        default:
-            arrlength>3?arrNum=arrNum:arrNum=e.target.value
-            // arrNum=e.target.value
-            console.log(`length: ${arrlength}`)
-    }
-   console.log(arrNum)
+    // isNaN(e.target.value)?alert():'' to see if the input is a number
+    numeros=`${e.target.value}`
+    svgnumber.textContent=`
+    ${numeros.slice(0,4)} ${numeros.slice(4,8)} ${numeros.slice(8,12)} ${numeros.slice(12,16)}
+    `;
+    e.target.value===``?svgnumber.textContent='0123 4567 8910 1112':'';
+
 })
+//Generate random card number from list of known test numbers
+const randomCard = function () {
+    let testCards = [
+        '4000056655665556',
+        '5200828282828210',
+        '371449635398431',
+        '6011000990139424',
+        '30569309025904',
+        '3566002020360505',
+        '6200000000000005',
+        '6759649826438453',
+    ];
+    let randomNumber = Math.floor(Math.random() * testCards.length);
+    cardnumber.value= testCards[randomNumber];
+    numeros=cardnumber.value
+    svgnumber.textContent=`
+    ${numeros.slice(0,4)} ${numeros.slice(4,8)} ${numeros.slice(8,12)} ${numeros.slice(12,16)}
+    `;
+}
+generatecard.addEventListener('click', function () {
+    randomCard();
+});
